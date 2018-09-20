f42d535e20aa396be423faf1490858bb2661ca96
cooper <serunde@gmail.com>
2018-09-19 17:05:02 -0700

    Refactor for kebab and product, styling overhaul
    State of `kebab` has been moved to `product`

--- packages/venia-concept/src/components/MiniCart/kebab.js
+++ packages/venia-concept/src/components/MiniCart/kebab.js
@@ -5,59 +5,49 @@ import classify from 'src/classify';
 import defaultClasses from './kebab.css';
 
 class Kebab extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isOpen: false
-        };
-    }
 
     render() {
-        const { classes, item, toggleKebab } = this.props;
-        const toggleClass = this.state.isOpen ? classes.dropdown + ' ' + classes.active : classes.dropdown;
-        const iconAttrs = { color: 'rgb(var(--venia-teal))'};
+        const { classes, item, isOpen } = this.props;
+        const toggleClass = isOpen ? classes.dropdown + ' ' + classes.active : classes.dropdown;
+        const iconAttrs = {
+              color: 'rgb(var(--venia-teal))',
+              width: '16px',
+              height: '16px'
+        };
 
         return (
-            <div className={classes.subMenu}>
-              <button onClick={this.openDropdown} onBlur={this.closeDropdown}>
-                <Icon name='more-vertical' attrs={iconAttrs} /> 
+            <div className={classes.root}>
+              <button>
+                <Icon name='more-vertical' attrs={{color: 'rgb(var(--venia-teal))'}} />
               </button>
-                <ul className={toggleClass} onFocus={this.openDropdown} onBlur={this.closeDropdown}>
+                <ul className={toggleClass} >
                   <li>
-                    <a href="/">
-                      <Icon name='heart' attrs={iconAttrs} />
-                      Add to favorites
-                    </a>
+                    <button>
+                      <div>
+                        <Icon name='heart' attrs={iconAttrs} />
+                        <span className={classes.text}>Add to favorites</span>
+                      </div>
+                    </button>
                   </li>
                   <li>
-                    <a href="/">
-                      <Icon name='edit-2' attrs={iconAttrs} />
-                      Edit item
-                    </a>
+                    <button>
+                      <div>
+                        <Icon name='edit-2' attrs={iconAttrs} />
+                        <span className={classes.text}>Edit item</span>
+                      </div>
+                    </button>
                   </li>
                   <li>
-                    <button onClick={() => this.removeItem(item)}>
-                      <Icon name='trash' attrs={iconAttrs}/>
-                      Remove item
+                    <button onClick={() => this.removeItem(item)} >
+                      <div>
+                        <Icon name='trash' attrs={iconAttrs} />
+                        <span className={classes.text}>Remove item</span>
+                      </div>
                     </button>
                   </li>
                 </ul>
             </div>
         )
-    }
-
-    openDropdown = () => {
-        this.setState({
-            isOpen: true
-        }); 
-        this.props.toggleKebab(this.state.isOpen);
-    }
-
-    closeDropdown = () => {
-        this.setState({
-            isOpen: false
-        });
-        this.props.toggleKebab(this.state.isOpen);
     }
 
     removeItem = (item) => {
